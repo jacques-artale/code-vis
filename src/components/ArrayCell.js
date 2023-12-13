9161eec202a2dfc97e2294ee8db47ad779535ec8
boblee <jacques.artale@hotmail.com>
2023-12-13 22:01:32 +0100

    add: integrated webpack

--- src/components/ArrayCell.js
+++ src/components/ArrayCell.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 function ArrayCell({ key, value }) {
   return (
     <div key={key} style={{width: '25px', height: '25px', border: '1px solid black', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
