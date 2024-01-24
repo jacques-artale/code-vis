f5c593098d2db3a04f53c2ef12e536e5ceb76be8
boblee <jacques.artale@hotmail.com>
2024-01-24 19:53:17 +0100

    add: new position system for scopes - in progress

--- src/components/VisualView.js
+++ src/components/VisualView.js
@@ -24,19 +24,73 @@ const VisualView = ({ scopes }) => {
   }, [scopes]);
 
   useEffect(() => {
-    const newPositions = [];
+    const newPositions = calculatePositions(scopes, scopeRefs);
+    setPositions(newPositions);
+  }, [scopeComponents, scopes]);
+
+  
+  function calculatePositions(scopes, scopeRefs) {
+    // count number of children with same distance to global (center)
+    const scopeDistances = findDistancesBFS(0, scopes); // distances[scopeId] = distance to global
+
+    // this will be the number of children in the same ring (placed with same distance to global in a circle)
+    const scopeDistancesCount = scopeDistances.reduce((acc, curr) => {
+      if (curr in acc) {
+        acc[curr]++;
+      } else {
+        acc[curr] = 1;
+      }
+      return acc;
+    }, {});
+    
+    
+    const positions = [];
+    
     for (const scope of scopes) {
       if (scopeRefs.current[scope.id].current) {
         const rect = scopeRefs.current[scope.id].current.getBoundingClientRect();
         const width = rect.width;
         const height = rect.height;
         
-        // Insert amazing algorithm to calculate positions here
-        newPositions[scope.id] = { x: width, y: height };
+        // the middle of each scope is the x,y position so we need to take into account the width and height
+
+        // calculate the angle of the scope
+        const childIndex = 0;
+        const radius = 400 * scopeDistances[scope.id];
+        const angle = 2 * Math.PI * childIndex / scopeDistancesCount[scopeDistances[scope.id]];
+
+        // calculate the x,y position of the scope
+        const x = Math.cos(angle) * radius;
+        const y = Math.sin(angle) * radius;
+
+        positions[scope.id] = { x: x, y: y };
       }
     }
-    setPositions(newPositions);
-  }, [scopeComponents]);
+
+    return positions;
+  }
+
+  function findDistancesBFS(globalScopeId, scopes) {
+    const distances = [];
+    distances[globalScopeId] = 0;
+
+    let queue = [globalScopeId];
+
+    while (queue.length > 0) {
+      let currentScopeId = queue.shift();
+
+      let children = scopes.filter(scope => scope.parentId === currentScopeId);
+
+      for (let child of children) {
+        if (!(child.id in distances)) {
+          distances[child.id] = distances[currentScopeId] + 1;
+          queue.push(child.id);
+        }
+      }
+    }
+
+    return distances;
+  }
 
   function createScopeComponent(scope) {
     return (
@@ -60,7 +114,7 @@ const VisualView = ({ scopes }) => {
   }
 
   return (
-    <div style={{ position: 'relative', width: '100%', height: '100%', border: '1px solid black' }}>
+    <div style={{ position: 'relative', width: '100%', height: '100%', border: '1px solid black', overflow: 'scroll' }}>
       {
         /* Scopes */
         scopeComponents.map((scopeComponent) => scopeComponent)
